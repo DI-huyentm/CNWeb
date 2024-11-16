096c1bbab79accd5954b3c978cf6ed8bb06fa003
ngoduckhanhz <khanhducngo1234@gmail.com>
2024-11-16 22:57:11 +0700

    service

--- BE/routes/statisticsRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const statisticsController = require("../controllers/statisticController");
-
-router.get("/", statisticsController.getTotalJobsAndAppliedJobsByIndustry);
-
-module.exports = router;
--- BE/routes/tagRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const tagController = require("../controllers/tagController");
-const router = express.Router();
-
-router.get("/", tagController.getAllTags);
-
-module.exports = router;
